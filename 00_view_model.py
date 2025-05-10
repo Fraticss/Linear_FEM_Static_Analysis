296ff66d5ac4c1be937421c8e5fc4e47d5a9c04b
TantardiniFrancesco <123182199+Fraticss@users.noreply.github.com>
2025-05-11 00:27:38 +0200

    Update 00_view_model.py

--- 00_view_model.py
+++ 00_view_model.py
@@ -31,7 +31,7 @@ gmsh.option.setNumber("Mesh.VolumeEdges", 0)
 gmsh.option.setNumber("Mesh.VolumeFaces", 0)
 
 # Set GUI background to black and text to white
-gmsh.option.setNumber("General.ColorScheme", 3)  # Set gradient color to black
+gmsh.option.setNumber("General.ColorScheme", 3)  # Set dark mode
 gmsh.option.setNumber("General.Light0", 0)  # Disable light
 
 # Start the gui for the mesh visualization
